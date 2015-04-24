cf7ca43016af3f47435627e9b70597755e8a3a46
7408hacks <74hc08@gmail.com>
2015-04-24 22:37:40 +0200

    Initial commit

---
+++ DualTerminal/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace DoubleTerminal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripComboBoxPort1 = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripComboBoxPort2 = new System.Windows.Forms.ToolStripComboBox();
+            this.refreshToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripComboBoxBaud = new System.Windows.Forms.ToolStripComboBox();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.serialPort2 = new System.IO.Ports.SerialPort(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(0, 27);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1095, 680);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripComboBoxPort1,
+            this.toolStripComboBoxPort2,
+            this.refreshToolStripMenuItem,
+            this.toolStripComboBoxBaud,
+            this.openToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1095, 27);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripComboBoxPort1
+            // 
+            this.toolStripComboBoxPort1.ForeColor = System.Drawing.Color.Red;
+            this.toolStripComboBoxPort1.Name = "toolStripComboBoxPort1";
+            this.toolStripComboBoxPort1.Size = new System.Drawing.Size(121, 23);
+            this.toolStripComboBoxPort1.Text = "Port1";
+            // 
+            // toolStripComboBoxPort2
+            // 
+            this.toolStripComboBoxPort2.ForeColor = System.Drawing.Color.Blue;
+            this.toolStripComboBoxPort2.Name = "toolStripComboBoxPort2";
+            this.toolStripComboBoxPort2.Size = new System.Drawing.Size(121, 23);
+            this.toolStripComboBoxPort2.Text = "Port2";
+            // 
+            // refreshToolStripMenuItem
+            // 
+            this.refreshToolStripMenuItem.Name = "refreshToolStripMenuItem";
+            this.refreshToolStripMenuItem.Size = new System.Drawing.Size(58, 23);
+            this.refreshToolStripMenuItem.Text = "Refresh";
+            this.refreshToolStripMenuItem.Click += new System.EventHandler(this.refreshToolStripMenuItem_Click);
+            // 
+            // toolStripComboBoxBaud
+            // 
+            this.toolStripComboBoxBaud.Items.AddRange(new object[] {
+            "300",
+            "600",
+            "1200",
+            "2400",
+            "9600",
+            "14400",
+            "19200",
+            "38400",
+            "57600",
+            "115200",
+            "250000",
+            "1000000"});
+            this.toolStripComboBoxBaud.Name = "toolStripComboBoxBaud";
+            this.toolStripComboBoxBaud.Size = new System.Drawing.Size(121, 23);
+            this.toolStripComboBoxBaud.Text = "Baud";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(48, 23);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Enabled = false;
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(48, 23);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 710);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1095, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(22, 17);
+            this.toolStripStatusLabel1.Text = "---";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(22, 17);
+            this.toolStripStatusLabel2.Text = "---";
+            // 
+            // serialPort2
+            // 
+            this.serialPort2.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort2_DataReceived);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1095, 732);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "DualTerminal";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripMenuItem refreshToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBoxPort1;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBoxPort2;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBoxBaud;
+        private System.IO.Ports.SerialPort serialPort2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+    }
+}
+
---
+++ DualTerminal/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+
+namespace DoubleTerminal
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            toolStripComboBoxBaud.SelectedIndex = 4;
+            int nPorts = refreshPorts();
+            if (nPorts >= 2)
+            {
+                toolStripComboBoxPort1.SelectedIndex = 0;
+                toolStripComboBoxPort2.SelectedIndex = 1;
+            }
+        }
+
+         // Append text of the given color.
+        private void AppendText(Color color, string text)
+        {
+            int start = richTextBox1.TextLength;
+            richTextBox1.AppendText(text);
+            int end = richTextBox1.TextLength;
+            
+            // Textbox may transform chars, so (end-start) != text.Length
+            richTextBox1.Select(start, end - start);
+            {
+                richTextBox1.SelectionColor = color;
+                // could set box.SelectionBackColor, box.SelectionFont too.
+            }
+            richTextBox1.SelectionLength = 0; // clear
+        }
+
+        private int refreshPorts()
+        {
+            string[] ArrayComPortsNames = null;
+            ArrayComPortsNames = SerialPort.GetPortNames();
+            toolStripComboBoxPort1.Items.Clear();
+            toolStripComboBoxPort2.Items.Clear();
+            toolStripComboBoxPort1.Items.AddRange(ArrayComPortsNames);
+            toolStripComboBoxPort2.Items.AddRange(ArrayComPortsNames);
+            return ArrayComPortsNames.Length;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.AppendText(Color.Blue,"dupa");
+        }
+
+        private void refreshToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            refreshPorts();
+        }
+
+        private void status()
+        {
+            if (serialPort1.IsOpen)
+            {
+                toolStripStatusLabel1.Text = serialPort1.PortName;
+                toolStripStatusLabel1.ForeColor = Color.Red;
+            }
+            else
+            {
+                toolStripStatusLabel1.Text = "---";
+                toolStripStatusLabel1.ForeColor = Color.Black;
+            }
+            if (serialPort2.IsOpen)
+            {
+                toolStripStatusLabel2.Text = serialPort2.PortName;
+                toolStripStatusLabel2.ForeColor = Color.Blue;
+            }
+            else
+            {
+                toolStripStatusLabel2.Text = "---";
+                toolStripStatusLabel2.ForeColor = Color.Black;
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender=null, EventArgs e=null)
+        {
+            try
+            {
+                serialPort1.PortName = toolStripComboBoxPort1.SelectedItem.ToString();
+                serialPort2.PortName = toolStripComboBoxPort2.SelectedItem.ToString();
+                serialPort1.Open();
+                serialPort2.Open();
+                openToolStripMenuItem.Enabled = false;
+                closeToolStripMenuItem.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                closeToolStripMenuItem_Click();
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                status();
+            }
+        }
+
+        private void closeToolStripMenuItem_Click(object sender=null, EventArgs e=null)
+        {
+            serialPort1.Close();
+            serialPort2.Close();
+            openToolStripMenuItem.Enabled = true;
+            closeToolStripMenuItem.Enabled = false;
+            status();
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            Invoke(new MethodInvoker(delegate() { AppendText(Color.Red, serialPort1.ReadExisting()); }));
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            closeToolStripMenuItem_Click();
+        }
+
+        private void serialPort2_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            Invoke(new MethodInvoker(delegate() { AppendText(Color.Blue, serialPort2.ReadExisting()); }));
+        }
+    }
+}
